3d0cde8cad726826b5eceebe0df49ce79f96ae8c
daniel01mps <daniel01mps@gmail.com>
2024-10-10 22:05:51 -0300

    Primeira aula

---
+++ index.js
@@ -0,0 +1,58 @@
+// // const numero = prompt('Digite um número:')
+// // if(numero > 0) {
+// //     document.write(`o ${numero} é maior que 0`);
+// // } else if (numero <0) {
+// //     document.write(`o ${numero} é menor que 0`);
+// // } else {
+// //     document.write(`o numero é igual a 0`);
+// // }
+
+// // const LadoA = prompt('Digite o Lado 1:')
+// // const LadoB = prompt('Digite o Lado 2:')
+// // const LadoC = prompt('Digite o Lado 3:')
+// // if(LadoA == LadoB && LadoB == LadoC){
+// //     document.write(`O triangulo é equilatero`);
+// // } else if(LadoA == LadoB || LadoA == LadoC || LadoB == LadoC){
+// //     document.write(`O triangulo é isosceles`);
+// // } else{
+// //     document.write(`O triangulo é escaleno`);
+// // }
+
+// const ano = prompt('Digite um numero:')
+// if(ano %4 == 0){
+// document.write(`${ano} é um ano bissexto`);
+// } else {
+//     document.write(`${ano} não é um ano bissexto`)
+// }
+
+// const primeiroNumero = prompt ('Digite o primeiro número:'); 
+// const segundoNumero = prompt ('Digite o segundo número:'); 
+// const terceiroiroNumero = prompt ('Digite o terceiro número:'); 
+// if(primeiroNumero > segundoNumero && primeiroNumero > terceiroiroNumero){
+//     document.write(`${primeiroNumero} é o maior número`);
+// }
+// else if(segundoNumero > primeiroNumero && segundoNumero > terceiroiroNumero){
+//     document.write(`${segundoNumero} é o maior número`);
+// }
+// else {
+//     document.write(`${terceiroiroNumero} é o maior número`);
+// }
+
+// const numero = prompt('Digite um numero:')
+// if(numero %2 == 0){
+// document.write(`${ numero} é par`);
+// } else {
+//     document.write(`${numero} é impar`);
+// }
+
+// const  preco = prompt(`Digite o Preco do produto`);
+// const  quantidade = prompt(`Digite a quantidade de produtos vendidos`);
+// let total;
+
+// if(quantidade > 10){
+//     total= quantidade * (preco - (preco *0.1));
+//     document.write(`o total a ser pago sera de R$${total}`)
+// } else {
+//     total = quantidade * preco;
+//     document.write(`o total a ser pago será de R$${total}`)
+// }
