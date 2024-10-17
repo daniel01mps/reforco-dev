8e819a0efabf6c988d8ff8e47227ec8841fb2859
daniel01mps <daniel01mps@gmail.com>
2024-10-17 19:22:09 -0300

    Desafios

--- index.js
+++ index.js
@@ -38,21 +38,82 @@
 //     document.write(`${terceiroiroNumero} é o maior número`);
 // }
 
-const numero = prompt('Digite um numero:')
-if(numero %2 == 0){
-document.write(`${ numero} é par`);
-} else {
-    document.write(`${numero} é impar`);
-}
+// const numero = prompt('Digite um numero:')
+// if(numero %2 == 0){
+// document.write(`${ numero} é par`);
+// } else {
+//     document.write(`${numero} é impar`);
+// }
 
-// const  preco = prompt(`Digite o Preco do produto`);
-// const  quantidade = prompt(`Digite a quantidade de produtos vendidos`);
+// const qtd = prompt('Digite a quantidade de produtos vendidos: ');
+// const preco = prompt('Digite o preco do produto');
 // let total;
 
-// if(quantidade > 10){
-//     total= quantidade * (preco - (preco *0.1));
-//     document.write(`o total a ser pago sera de R$${total}`)
+// if(qtd > 10){
+//     total = qtd * (preco - (preco * 0.1));
+//     document.write(`O total a ser pago sera de R$${total}`)
 // } else {
-//     total = quantidade * preco;
-//     document.write(`o total a ser pago será de R$${total}`)
+//     total = qtd * preco;
+//     document.write(`O total a ser pago sera de R$${total}`)
+// }
+
+// const nota = prompt('Digite a nota:');
+
+// if (nota > 0) {
+//     document.write(`O conceito da sua ${nota} é F`);
+// } else if (nota > 20) {
+//     document.write(`O conceito da sua ${nota} é D`);
+// } else if (nota > 40) {
+//     document.write(`O conceito da sua ${nota} é C`);
+// } else if (nota > 60) {
+//     document.write(`O conceito da sua ${nota} é B`);
+// } else if (nota > 80 && nota <= 100) {
+//     document.write(`O conceito da sua ${nota} é A`);
+// }
+
+// let letra = prompt ('Digite uma letra');
+// let vogais = 'AEIOUaeiou';
+// let consoantes = 'BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz';
+// vogais = vogais.split('');
+// consoantes = consoantes.split('');
+
+// console.log(vogais)
+// if(vogais.includes(letra)){
+//     document.write(`a letra ${letra} é uma vogal`);
+// } else if (consoantes.includes(letra)){  
+//     document.write(`a letra ${letra} é uma consoante`);
+//     } else{
+//         alert(`o caracter ${letra} não é uma letra`);
+//     }
+
+
+// let dia = prompt ('Digite um número');
+// if(dia == '1'){
+//     document.write(`Domingo`);
+// }else if(Dia == '2'){
+//     document.write(`Segunda-Feira`);
+// }else if(Dia == '3'){
+//     document.write(`Terca-Feira`);
+// }else if(Dia == '4'){
+//     document.write(`Quarta-Feira`);
+// }else if(Dia == '5'){
+//     document.write(`Quinta-Feira`);
+// }else if(Dia == '6'){
+//     document.write(`Sexta-Feira`);
+// }else if(Dia == '7'){
+//     document.write(`Sábado`);
+// // }
+        
+// const valor = prompt('Digite sua renda anual:');
+
+// if (valor >= 24511.93 && valor <= 33919.80){
+//     document.write(`Seu imposto de renda é de R$ 1838.39`);
+// } else if (valor >= 33919.81 && valor <= 45012.60){
+//     document.write(`Seu imposto de renda é de R$ 4382.38`);
+// } else if (valor >= 45012.61 && valor <= 55976.16){
+//     document.write(`Seu imposto de renda é de R$ 7758.32`);
+// } else if (valor > 55976.16){
+//     document.write(`Seu imposto de renda é de R$ 10557.13`);
+// } else{
+//     document.write(`Você é isento`)
 // }
